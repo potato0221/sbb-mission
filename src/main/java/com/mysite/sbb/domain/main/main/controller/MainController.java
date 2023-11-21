834d91e8c458416c945537abfb6b484ea20dabff
potato0221 <wnqo10@naver.com>
2023-11-21 11:34:27 +0900

    2-08

--- src/main/java/com/mysite/sbb/domain/main/main/controller/MainController.java
+++ src/main/java/com/mysite/sbb/domain/main/main/controller/MainController.java
@@ -1,4 +1,4 @@
-package com.mysite.sbb;
+package com.mysite.sbb.domain.main.main.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,5 +10,10 @@ public class MainController {
     @ResponseBody
     public String index(){
         return "안녕하세요 sbb에 오신것을 환영합니다.";
+    }
+
+    @GetMapping("/")
+    public String root(){
+        return "redirect:/question/list";
     }
 }
