a79ca06622dfbbac8d51d92a594dbaaa3643ef73
potato0221 <wnqo10@naver.com>
2023-11-21 14:16:27 +0900

    2-10

--- src/main/java/com/mysite/sbb/domain/question/question/service/QuestionService.java
+++ src/main/java/com/mysite/sbb/domain/question/question/service/QuestionService.java
@@ -1,11 +1,13 @@
 package com.mysite.sbb.domain.question.question.service;
 
+import com.mysite.sbb.DataNotFoundException;
 import com.mysite.sbb.domain.question.question.entitiy.Question;
 import com.mysite.sbb.domain.question.question.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,5 +16,14 @@ public class QuestionService {
 
     public List<Question> getList(){
         return this.questionRepository.findAll();
+    }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question=this.questionRepository.findById(id);
+        if(question.isPresent()){
+            return question.get();
+        }else {
+            throw new DataNotFoundException("question is not found");
+        }
     }
 }
