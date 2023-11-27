9e123e94c555cdc8fc9d8c4e49f7b82a37de1878
potato0221 <wnqo10@naver.com>
2023-11-27 11:18:51 +0900

    3-07, 로그인,로그아웃 버튼 숨기기 해결 안됨

--- src/main/java/com/mysite/sbb/global/initdata/NotProd.java
+++ src/main/java/com/mysite/sbb/global/initdata/NotProd.java
@@ -3,6 +3,8 @@ package com.mysite.sbb.global.initdata;
 import com.mysite.sbb.domain.question.question.entitiy.Question;
 import com.mysite.sbb.domain.question.question.repository.QuestionRepository;
 import com.mysite.sbb.domain.question.question.service.QuestionService;
+import com.mysite.sbb.domain.user.user.entitiy.SiteUser;
+import com.mysite.sbb.domain.user.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
@@ -16,7 +18,8 @@ public class NotProd {
 //    페이징을 위해 대량 테스트 데이터 추가
     @Bean
     public ApplicationRunner initNotProd(
-            QuestionService questionService
+            QuestionService questionService,
+            UserService userService
     ){
         return args ->  {
             for (int i = 1; i <= 60; i++) {
@@ -25,6 +28,10 @@ public class NotProd {
                         "질문 제목 " + i,
                         "질문 내용 " + i
                 );
+            }
+            for (int i=1;i<=3;i++){
+                SiteUser siteUser=new SiteUser();
+                userService.create("user"+i,"www@email.com"+i,"12345" );
             }
         };
     }
