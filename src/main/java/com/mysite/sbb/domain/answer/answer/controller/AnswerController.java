6a4b6910d2fc7bcc34f57cfa68902300cd1e3999
potato0221 <wnqo10@naver.com>
2023-11-28 13:13:07 +0900

    3-12

--- src/main/java/com/mysite/sbb/domain/answer/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/domain/answer/answer/controller/AnswerController.java
@@ -43,8 +43,8 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question/question/question_detail";
         }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer=this.answerService.create(question,answerForm.getContent(),siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -82,7 +82,7 @@ public class AnswerController {
         }
 
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(), answer.getId());
 
     }
 
@@ -111,7 +111,7 @@ public class AnswerController {
         Answer answer=this.answerService.getAnswer(id);
         SiteUser siteUser=this.userService.getUser(principal.getName());
         this.answerService.vote(answer,siteUser);
-        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s",answer.getQuestion().getId(), answer.getId());
     }
 
 
--- src/main/java/com/mysite/sbb/domain/answer/answer/service/AnswerService.java
+++ src/main/java/com/mysite/sbb/domain/answer/answer/service/AnswerService.java
@@ -17,7 +17,7 @@ public class AnswerService {
 
     private final AnswerRepository answerRepository;
 
-    public void create(Question question, String content, SiteUser author){
+    public Answer create(Question question, String content, SiteUser author){
         Answer answer=new Answer();
 
         answer.setContent(content);
@@ -25,6 +25,7 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+        return answer;
     }
 
     public Answer getAnswer(Integer id){
