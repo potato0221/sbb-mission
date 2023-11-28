3c97c2598331b022f702d6d11d423b23c68ba4d6
potato0221 <wnqo10@naver.com>
2023-11-28 20:52:19 +0900

    @Profile 추가

--- src/main/java/com/mysite/sbb/global/initdata/NotProd.java
+++ src/main/java/com/mysite/sbb/global/initdata/NotProd.java
@@ -9,7 +9,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
+@Profile("!prod")
 @Configuration
 public class NotProd {
     @Autowired
