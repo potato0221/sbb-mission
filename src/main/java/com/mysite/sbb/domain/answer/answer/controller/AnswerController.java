97297a79c5875c5995bbbcb2ca60a19ce2834562
potato0221 <wnqo10@naver.com>
2023-11-28 10:55:24 +0900

    3-10

--- src/main/java/com/mysite/sbb/domain/answer/answer/controller/AnswerController.java
+++ src/main/java/com/mysite/sbb/domain/answer/answer/controller/AnswerController.java
@@ -1,6 +1,7 @@
 package com.mysite.sbb.domain.answer.answer.controller;
 
 import com.mysite.sbb.domain.answer.answer.dto.AnswerForm;
+import com.mysite.sbb.domain.answer.answer.entity.Answer;
 import com.mysite.sbb.domain.answer.answer.service.AnswerService;
 import com.mysite.sbb.domain.question.question.entitiy.Question;
 import com.mysite.sbb.domain.question.question.service.QuestionService;
@@ -8,13 +9,16 @@ import com.mysite.sbb.domain.user.user.entitiy.SiteUser;
 import com.mysite.sbb.domain.user.user.service.UserService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.security.Principal;
 
@@ -26,19 +30,76 @@ public class AnswerController {
     private final QuestionService questionService;
     private final AnswerService answerService;
     private final UserService userService;
+
     @PreAuthorize("isAuthenticated()")
     @PostMapping("/create/{id}")
     public String createAnswer(Model model,
                                @PathVariable("id") Integer id,
                                @Valid AnswerForm answerForm, BindingResult bindingResult,
-                               Principal principal){
-        Question question=this.questionService.getQuestion(id);
-        SiteUser siteUser=this.userService.getUser(principal.getName());
-        if(bindingResult.hasErrors()){
-            model.addAttribute("question",question);
+                               Principal principal) {
+        Question question = this.questionService.getQuestion(id);
+        SiteUser siteUser = this.userService.getUser(principal.getName());
+        if (bindingResult.hasErrors()) {
+            model.addAttribute("question", question);
             return "question/question/question_detail";
         }
-        this.answerService.create(question,answerForm.getContent(),siteUser);
-        return String.format("redirect:/question/detail/%s",id);
+        this.answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s", id);
     }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/modify/{id}")
+    public String answerModify(
+            AnswerForm answerForm,
+            @PathVariable("id") Integer id,
+            Principal principal){
+
+        Answer answer=this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정 권한이 없습니다.");
+        }
+
+        answerForm.setContent(answer.getContent());
+        return "answer/answer/answer_form";
+
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @PostMapping("/modify/{id}")
+    public String answerModify(
+            @Valid AnswerForm answerForm,
+            BindingResult bindingResult,
+            @PathVariable("id") Integer id,
+            Principal principal){
+
+        if(bindingResult.hasErrors()){
+            return "answer/answer/answer_form";
+        }
+        Answer answer=this.answerService.getAnswer(id);
+
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"수정 권한이 없습니다.");
+        }
+
+        this.answerService.modify(answer, answerForm.getContent());
+        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+
+    }
+
+    @PreAuthorize("isAuthenticated()")
+    @GetMapping("/delete/{id}")
+    public String answerDelete(
+            Principal principal,
+            @PathVariable("id") Integer id){
+
+        Answer answer=this.answerService.getAnswer(id);
+        if(!answer.getAuthor().getUsername().equals(principal.getName())){
+            throw new ResponseStatusException(HttpStatus.BAD_REQUEST,"삭제 권한이 없습니다.");
+        }
+
+        this.answerService.delete(answer);
+
+        return String.format("redirect:/question/detail/%s",answer.getQuestion().getId());
+    }
+
 }
--- src/main/java/com/mysite/sbb/domain/answer/answer/service/AnswerService.java
+++ src/main/java/com/mysite/sbb/domain/answer/answer/service/AnswerService.java
@@ -4,10 +4,12 @@ import com.mysite.sbb.domain.answer.answer.entity.Answer;
 import com.mysite.sbb.domain.answer.answer.repository.AnswerRepository;
 import com.mysite.sbb.domain.question.question.entitiy.Question;
 import com.mysite.sbb.domain.user.user.entitiy.SiteUser;
+import com.mysite.sbb.global.exception.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -23,6 +25,25 @@ public class AnswerService {
         answer.setQuestion(question);
         answer.setAuthor(author);
         this.answerRepository.save(answer);
+    }
+
+    public Answer getAnswer(Integer id){
+        Optional<Answer> answer=this.answerRepository.findById(id);
+        if(answer.isPresent()){
+            return answer.get();
+        }else {
+            throw new DataNotFoundException("answer not found");
+        }
+    }
+
+    public void modify(Answer answer, String content){
+        answer.setContent(content);
+        answer.setModifyDate(LocalDateTime.now());
+        this.answerRepository.save(answer);
+    }
+
+    public void delete(Answer answer){
+        this.answerRepository.delete(answer);
     }
 
 }
